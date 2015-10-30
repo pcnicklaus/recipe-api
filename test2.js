1406e30fa67e28046fd5222456e37c17ef7debf3
Patrick <pcnicklaus@gmail.com>
2015-10-30 07:29:39 -0600

    removed keys to be safe

--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
  $.ajax({
-    url: "http://food2fork.com/api/search?key=4ee21f110453547468b252d74f4b92c0&",
+    url: "http://food2fork.com/api/search?key=..",
     headers: {"X-Test-Header": "test-value"},
     method: "GET",
   });
